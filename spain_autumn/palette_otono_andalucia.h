c6227ec95f8279a69134237cae0bee67d2c73bc1
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2025-01-11 23:06:17 +0100

    Create palette_otono_andalucia.h

---
+++ spain_autumn/palette_otono_andalucia.h
@@ -0,0 +1,71 @@
+// palette_otono_andalucia.h
+// Expanded color palette inspired by Andalucía's autumn, organized by subregions
+
+#ifndef PALETTE_OTONO_ANDALUCIA_H
+#define PALETTE_OTONO_ANDALUCIA_H
+
+// --- GENERAL ANDALUCÍA COLORS ---
+#define NARANJA_SEVILLA   0xFB60  // Seville orange
+#define VERDE_OLIVA       0x8404  // Olive green from Andalusian groves
+#define AZUL_ALHAMBRA     0x4C3F  // Blue of Alhambra tiles
+#define OCRE_ALPUJARRAS   0xDA40  // Ochre tones from the Alpujarras hills
+#define ROJO_CORDOBA      0xA808  // Crimson inspired by Córdoba patios
+
+// --- SUBREGIONS ---
+// --- ALMERÍA ---
+#define OCRE_CABO_GATA    0xDB40  // Ochre cliffs of Cabo de Gata
+#define AZUL_MEDITERRANEO 0x5D3F  // Mediterranean blue along the coast
+#define AMARILLO_ARENA    0xFEC0  // Golden sand of Almería’s beaches
+#define VERDE_DESIERTO    0x6A04  // Green shrubs in the Tabernas desert
+#define GRIS_SIERRA_ALHAMILLA 0x8C73  // Stone gray from the Sierra Alhamilla
+
+// --- GRANADA ---
+#define ROJO_ARABE        0xA807  // Deep red inspired by Nasrid art
+#define VERDE_MUSGO       0x6406  // Moss green from Sierra Nevada forests
+#define OCRE_ALPUJARRAS   0xDA40  // Ochre tones from Alpujarras terraced farms
+#define GRIS_NEVADA       0x8C75  // Gray of Sierra Nevada’s rocky peaks
+#define BLANCO_SIERRA     0xFFFF  // Snow white from Sierra Nevada
+
+// --- SEVILLA ---
+#define NARANJA_SEVILLA   0xFB60  // Seville orange
+#define AMARILLO_GIRASOL  0xFEA0  // Sunflower yellow from the countryside
+#define VERDE_PARQUE_MARIA_LUISA 0x3C07  // Lush green from María Luisa Park
+#define GRIS_GIRALDA      0x9CF3  // Soft gray inspired by the Giralda tower
+#define AMBAR_TORRE_DEL_ORO 0xFA60  // Amber gold from Torre del Oro
+
+// --- MÁLAGA ---
+#define AZUL_CALA_MIJAS   0x4D1F  // Turquoise blue of Mijas coves
+#define AMARILLO_PLAYAS   0xFEC0  // Bright yellow sand from Málaga beaches
+#define VERDE_PINSAPO     0x3206  // Green from Sierra de las Nieves fir trees
+#define GRIS_MONTES       0x8C70  // Gray rocks of the Montes de Málaga
+#define ROJO_VINO_RONDA   0xB010  // Wine red from Ronda’s vineyards
+
+// --- CÓRDOBA ---
+#define ROJO_CORDOBA      0xA808  // Crimson from Córdoba’s patios
+#define AMARILLO_MEZQUITA 0xFEC2  // Golden hues from the Mezquita arches
+#define VERDE_OLIVARES    0x8404  // Olive green from vast Córdoba groves
+#define GRIS_CALIFAL      0x9CD4  // Gray from Caliphal-era architecture
+#define OCRE_GUADALQUIVIR 0xDA62  // Ochre tones of the Guadalquivir riverbanks
+
+// --- CÁDIZ ---
+#define AZUL_ATLANTICO    0x5C5F  // Deep blue from the Atlantic Ocean
+#define BLANCO_CASCO_ANTIGUO 0xFFFF  // White walls of Cádiz’s old town
+#define VERDE_PARQUE_DOÑANA 0x3A27  // Lush green from Doñana National Park
+#define GRIS_SALINAS      0x94B3  // Gray from the salt flats
+#define AMARILLO_ARENA_LA_BARROSA 0xFEA0  // Bright yellow from La Barrosa Beach
+
+// --- JAÉN ---
+#define VERDE_ACEITUNA    0x7C05  // Olive green from Jaén’s groves
+#define AMARILLO_TRIGO    0xFEC0  // Golden wheat fields
+#define OCRE_MONTES       0xDA40  // Ochre mountains
+#define GRIS_SIERRA_CAZORLA 0x8C76  // Gray from Sierra de Cazorla peaks
+#define BLANCO_NIEBLA     0xEF5D  // Misty white from Jaén’s foggy mornings
+
+// --- HUELVA ---
+#define ROJO_FRESAS       0xB810  // Strawberry red from Huelva farms
+#define VERDE_PINARES     0x3206  // Pine green from Huelva’s forests
+#define GRIS_MINAS_RIO_TINTO 0x8D92  // Gray from Río Tinto mines
+#define OCRE_RIO_TINTO    0xDA22  // Ochre tones of Río Tinto
+#define AZUL_ISLANTILLA   0x4C3E  // Deep blue from Islantilla beaches
+
+#endif // PALETTE_OTONO_ANDALUCIA_H
