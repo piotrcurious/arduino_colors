03ab3465f4ab16f826883f1ba9793af215b8b691
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2025-01-12 20:20:47 +0100

    Create palette_otono_andalucia2.h

---
+++ spain_autumn/palette_otono_andalucia2.h
@@ -0,0 +1,71 @@
+// palette_otono_andalucia.h  
+// Expanded color palette inspired by Andalucía's autumn, organized by provinces  
+
+#ifndef PALETTE_OTONO_ANDALUCIA_H  
+#define PALETTE_OTONO_ANDALUCIA_H  
+
+// --- GENERAL ANDALUCÍA COLORS ---  
+#define OCRE_ALHAMBRA         0xDA60  // Ochre from the Alhambra’s stone walls  
+#define AZUL_MEDITERRANEO     0x4D5F  // Mediterranean blue along the coastline  
+#define VERDE_OLIVARES        0x3A07  // Olive green from vast olive groves  
+#define AMARILLO_LA_MEZQUITA  0xFEB0  // Golden tones from the Mezquita in Córdoba  
+#define ROJO_FLAMENCO         0xB812  // Deep red from flamenco dresses  
+
+// --- SUBREGIONS ---  
+// --- ALMERÍA ---  
+#define OCRE_DESIERTO_TABERNAS 0xDB20  // Ochre tones from the Tabernas Desert  
+#define VERDE_CABO_DE_GATA     0x3B07  // Green from Cabo de Gata's sparse vegetation  
+#define AZUL_CALAS             0x4D7F  // Turquoise blue from hidden coves  
+#define GRIS_SIERRA_ALHAMILLA  0x8C74  // Gray from Sierra Alhamilla mountains  
+#define ROJO_PIMIENTOS         0xB810  // Red from Almería’s autumn pepper harvest  
+
+// --- CÁDIZ ---  
+#define AZUL_PLAYA_LA_CALLETA  0x5C7F  // Deep blue from La Caleta beach  
+#define VERDE_PARQUE_ALCORNOCALES 0x3A47 // Green from Los Alcornocales forest  
+#define OCRE_DUNAS_BOLONIA     0xDA60  // Sandy ochre from the Bolonia dunes  
+#define ROJO_ATUN_RONQUEO      0xB010  // Red from the tuna ronqueo process  
+#define AMARILLO_ALAMEDAS      0xFEC2  // Yellow leaves of Alameda trees in autumn  
+
+// --- CÓRDOBA ---  
+#define OCRE_MEZQUITA          0xDB40  // Golden ochre from the Mezquita’s arches  
+#define VERDE_SIERRA_MORENA    0x3C07  // Green from Sierra Morena forests  
+#define ROJO_PIMENTON          0xB808  // Red from autumn paprika  
+#define GRIS_PATIOS            0x8C73  // Gray tones from Córdoba’s stone patios  
+#define AZUL_GUADALQUIVIR      0x4C5F  // Blue from the Guadalquivir River  
+
+// --- GRANADA ---  
+#define OCRE_ALHAMBRA_GRANADA  0xDA40  // Ochre from the Alhambra in Granada  
+#define VERDE_ALPUJARRAS       0x3B27  // Green from Alpujarras valleys  
+#define ROJO_GRANADAS          0xB810  // Red from pomegranates, the symbol of Granada  
+#define AZUL_SIERRA_NEVADA     0x5D5F  // Crisp blue from Sierra Nevada skies  
+#define GRIS_MIRADOR_SAN_NICOLAS 0x8C74 // Gray from Mirador San Nicolás cobblestones  
+
+// --- HUELVA ---  
+#define VERDE_PARQUE_DONANA    0x3A27  // Green from Doñana National Park  
+#define OCRE_MINAS_RIO_TINTO   0xDA22  // Ochre tones from Río Tinto mines  
+#define AZUL_ATLANTICO         0x4C7F  // Atlantic blue from the Costa de la Luz  
+#define ROJO_FRESAS            0xA810  // Strawberry red from Huelva's fields  
+#define AMARILLO_HOJAS_PINARES 0xFEC3  // Yellow pine leaves in autumn  
+
+// --- JAÉN ---  
+#define VERDE_OLIVA            0x3C07  // Olive green from Jaén’s vast groves  
+#define OCRE_CERROS_UBEDA      0xDA40  // Ochre tones from the hills of Úbeda  
+#define ROJO_ESPARTO           0xB808  // Red from esparto grass during sunset  
+#define GRIS_CASTILLO_SANTA    0x8C73  // Gray from Castillo de Santa Catalina  
+#define AZUL_CIELO_LIMPIO      0x5D3F  // Clear blue skies over Jaén  
+
+// --- MÁLAGA ---  
+#define OCRE_ALCAZABA          0xDA60  // Ochre from Málaga’s Alcazaba  
+#define VERDE_PARQUE_MALAGA    0x3B27  // Green from Málaga’s city park  
+#define AZUL_CUEVA_NERJA       0x4C5F  // Blue hues from Nerja caves  
+#define ROJO_SARDINAS_ESPETO   0xB010  // Red from sardine espeto (skewers)  
+#define AMARILLO_LIMONES_AXARQUIA 0xFED0 // Lemon yellow from Axarquía’s fields  
+
+// --- SEVILLA ---  
+#define OCRE_GIRALDA           0xDB40  // Ochre tones from the Giralda tower  
+#define ROJO_CAPOTE_TORERO     0xB810  // Red from a bullfighter’s cape  
+#define VERDE_JARDINES_MARIA_LUISA 0x3C27 // Green from María Luisa Park  
+#define AZUL_RIO_GUADALQUIVIR  0x4C5F  // Blue from Guadalquivir River  
+#define AMARILLO_ALFARERIA     0xFEB2  // Yellow from Triana’s pottery  
+
+#endif // PALETTE_OTONO_ANDALUCIA_H
